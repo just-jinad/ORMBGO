32494e1c187a8bd45feade16d3f92582b7b84767
Jinad Tope Isaac <jinadtope66@gmail.com>
2024-07-31 23:49:23 +0100

    made a few changes to ensure user experience

---
+++ src/components/material-tailwind.js
@@ -0,0 +1,3 @@
+import { ThemeProvider, Button, Carousel } from "@material-tailwind/react";
+
+export { ThemeProvider, Button, Carousel };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = withMT({
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {},
   plugins: [],
