c203afddafbcc67d37997ad6526385060eb065fe
Jinad Tope Isaac <jinadtope66@gmail.com>
2024-07-31 10:14:31 +0100

    updated

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,15 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, ReactElement } from 'react';
 import { ThemeProvider } from '@material-tailwind/react'; // Adjust this import according to your actual ThemeProvider location
 
 interface LayoutProps {
-  children: ReactNode;
+  children: ReactElement | ReactElement[];
 }
 
 export function Layout({ children }: LayoutProps) {
   if (!children) {
     throw new Error("Children must be provided to the Layout component");
   }
-  
+
   return <ThemeProvider>{children}</ThemeProvider>;
 }
 
