2542f0b4a7effdea56b9c150424302d5a99fd808
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-07-31 10:27:11 +0100

    updated

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,16 @@
 import React, { ReactNode } from 'react';
-import { ThemeProvider } from '@material-tailwind/react'; // Adjust this import according to your actual ThemeProvider location
+import { ThemeProvider } from '@material-tailwind/react';
 
 interface LayoutProps {
-  children: ReactNode | null | undefined;
+  children: ReactNode;
 }
 
 export function Layout({ children }: LayoutProps) {
-  return <ThemeProvider>{children ?? <></>}</ThemeProvider>;
+  if (typeof children !== 'string' && typeof children !== 'number' && typeof children !== 'boolean' && typeof children !== 'bigint' && React.isValidElement(children) || (Array.isArray(children) && children.every(child => React.isValidElement(child)))) {
+    return <ThemeProvider>{children}</ThemeProvider>;
+  } else {
+    return <ThemeProvider>{null}</ThemeProvider>;
+  }
 }
 
 export default Layout;
