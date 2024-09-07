d19d6f3457750923375ce74740afe03bb57ca3a1
Jinad Tope Isaac <jinadtope66@gmail.com>
2024-09-07 20:55:58 +0100

    About page updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,17 +2,21 @@ import { Navbar } from "@/components";
 import FooterII from "@/components/footerII";
 import React from "react";
 
-const page = () => {
+const AboutPage = () => {
   return (
     <>
       <Navbar />
       <div className="bg-gray-400 py-8 sm:py-12">
         {/* About Us Section */}
         <section className="text-center py-12 sm:py-16">
-          <h1 className="text-2xl sm:text-3xl font-bold mb-4">About Us</h1>
+          <h1 className="text-2xl sm:text-3xl font-bold mb-4">Who We Are</h1>
           <p className="text-gray-600 max-w-xl mx-auto px-4 sm:px-0">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            One Root Many Branches Global Outreach, or ORMBGO, is a non-profit
+            community service organization headquartered in Boca Raton, Florida,
+            USA. Our primary aim is to raise awareness in local communities of
+            the needs of individuals who are isolated from their families, either
+            permanently or temporarily, due to internment in nursing homes, rehab
+            facilities, or isolation at home.
           </p>
         </section>
 
@@ -23,9 +27,9 @@ const page = () => {
               Our Mission
             </h2>
             <p className="text-gray-600 text-center max-w-3xl mx-auto mb-8">
-              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-              consectetur, adipisci velit, sed quia non numquam eius modi tempora
-              incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+              We connect senior citizens who reside in nursing homes with the local
+              community and help reduce the impact of loneliness through weekly
+              interactive programs and special events.
             </p>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               {/* Images for mission */}
@@ -36,14 +40,31 @@ const page = () => {
           </div>
         </section>
 
-        {/* Meet Our Leaders Section */}
+        {/* Vision Section */}
+        <section className="py-12 sm:py-16 bg-gray-100">
+          <div className="max-w-5xl mx-auto px-4 sm:px-0">
+            <h2 className="text-xl sm:text-2xl font-bold text-center mb-8">
+              Our Vision
+            </h2>
+            <p className="text-gray-600 text-center max-w-3xl mx-auto mb-8">
+              To bring our program to every nursing home and rehabilitation
+              facility in Boca Raton, then expand into all 50 states, the Caribbean
+              Islands, and ultimately take our mission worldwide, one country at a
+              time.
+            </p>
+          </div>
+        </section>
+
+        {/* Meet Our Team Section */}
         <section className="bg-gray-100 py-12 sm:py-16">
           <h2 className="text-xl sm:text-2xl font-bold text-center mb-8">
-            Meet Our Leaders
+            Meet Our Team
           </h2>
           <p className="text-gray-600 text-center max-w-3xl mx-auto mb-8 sm:mb-12">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-            dolore eu fugiat nulla pariatur.
+            Our team of talented and dedicated volunteers utilize their talents and
+            abilities to bring love and cheer to our precious Senior Citizens as
+            well as the community at large. We have developed highly energetic and
+            enthusiastic programs to uplift those in need.
           </p>
 
           <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 text-center px-4 sm:px-0">
@@ -58,21 +79,21 @@ const page = () => {
             <div>
               <div className="bg-gray-300 rounded-full w-24 h-24 mx-auto mb-4"></div>
               <h3 className="font-bold">Tammy Johnson</h3>
-              <p className="text-gray-500">CEO</p>
+              <p className="text-gray-500">Director</p>
             </div>
 
             {/* Leader 3 */}
             <div>
               <div className="bg-gray-300 rounded-full w-24 h-24 mx-auto mb-4"></div>
               <h3 className="font-bold">David Hackett</h3>
-              <p className="text-gray-500">CEO</p>
+              <p className="text-gray-500">Volunteer Coordinator</p>
             </div>
 
             {/* Leader 4 */}
             <div>
               <div className="bg-gray-300 rounded-full w-24 h-24 mx-auto mb-4"></div>
               <h3 className="font-bold">Pamela Wagner</h3>
-              <p className="text-gray-500">CEO</p>
+              <p className="text-gray-500">Program Manager</p>
             </div>
           </div>
 
@@ -89,4 +110,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default AboutPage;
