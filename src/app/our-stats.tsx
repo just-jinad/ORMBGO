07c7f858f052c5d844b7cdfb6005dbad1fd02aef
Jinad Tope Isaac <jinadtope66@gmail.com>
2024-09-07 21:32:46 +0100

    update

--- src/app/our-stats.tsx
+++ src/app/our-stats.tsx
@@ -32,7 +32,7 @@ export function OurStats() {
       {/* Header section */}
       <div className="text-center mb-10">
         <h1 className="text-4xl font-bold leading-tight text-orange-500">
-          STORIES (coming soon)
+          OUR BLOGS (coming soon)
         </h1>
         <p className="mt-3 text-gray-500">
           Here we will be sharing heartwarming stories and inspiring experiences
