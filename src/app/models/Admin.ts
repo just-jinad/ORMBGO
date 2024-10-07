ddfe25e0e5df242a6bcb07e343aa493731c27000
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-10-07 14:22:30 +0100

    created the admin acct

--- src/app/models/Admin.ts
+++ src/app/models/Admin.ts
@@ -1,22 +1,22 @@
-import mongoose, { Document, Schema } from 'mongoose';
+// import mongoose, { Document, Schema } from 'mongoose';
 
-interface IAdmin extends Document {
-  username: string;
-  password: string;
-}
+// interface IAdmin extends Document {
+//   username: string;
+//   password: string;
+// }
 
-const AdminSchema: Schema<IAdmin> = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+// const AdminSchema: Schema<IAdmin> = new Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+// });
 
-const Admin = mongoose.models.Admin || mongoose.model<IAdmin>('Admin', AdminSchema);
+// const Admin = mongoose.models.Admin || mongoose.model<IAdmin>('Admin', AdminSchema);
 
-export default Admin;
+// export default Admin;
--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -8,7 +8,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 async function dbConnect() {
   if (mongoose.connection.readyState === 0) {
     try {
-      await mongoose.connect(MONGODB_URI as any);
+      await mongoose.connect(MONGODB_URI as string);
       console.log("MongoDB connected successfully");
     } catch (error) {
       console.error('MongoDB connection error:', error);
