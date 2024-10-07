0bb1928e7eadc4a4d521d30f980bbbbf5ec5bc50
Jinad Tope Isaac <jinadtope66@gmailcom>
2024-10-07 12:45:11 +0100

    update

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -1,16 +1,15 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI as string || 'mongodb://localhost:27017/ormbgo';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ormbgo';
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable in .env.local');
 }
 
 let cached = (global as any).mongoose;
-// let cached = global.mongoose;
 
 if (!cached) {
-    cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
@@ -24,14 +23,14 @@ async function dbConnect() {
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-        console.log("mongoose connected successfully");
-        
+      console.log("mongoose connected successfully");
       return mongoose;
     }).catch((error) => {
-        console.error('MongoDB connection error:', error);
-      });
+      console.error('MongoDB connection error:', error);
+    });
   }
   cached.conn = await cached.promise;
   return cached.conn;
 }
+
 export default dbConnect;
