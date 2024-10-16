a7a8aa66439098d22ecca99a5cce785ae5f57a75
Jinad Tope Isaac <jinadtope66@gmail.com>
2024-10-16 15:39:38 +0100

    updated the img to next Image'

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -1,20 +1,33 @@
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import {toast} from 'sonner'
+import { useState, useEffect } from 'react';
+import { toast } from 'sonner';
+
+const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      toast.error('Enter login details');
+      router.push('/login');
+    } else {
+      toast.success('Welcome');
+      setIsAuthenticated(true);
+    }
+  }, [router]);
+
+  return isAuthenticated;
+};
 
 const withAuth = (WrappedComponent: any) => {
-  return (props:any) => {
-    const router = useRouter();
+  return (props: any) => {
+    const isAuthenticated = useAuth();
 
-    useEffect(() => {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        toast.error('Enter login details')
-        router.push('/login'); 
-      }else{
-        toast.success('Welcome')
-      }
-    }, []);
+    if (!isAuthenticated) {
+      // While the check is ongoing or if unauthenticated, you can return null or a loader
+      return null;
+    }
 
     return <WrappedComponent {...props} />;
   };
